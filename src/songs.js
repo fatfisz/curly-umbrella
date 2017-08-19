cd770d0152dc2f312f3107e8a5ec1e0ff66c968b
fatfisz <fatfisz@gmail.com>
2017-08-19 02:35:37 +0200

    Separate the samples from the song

--- src/songs.js
+++ src/songs.js
@@ -1,4 +1,4 @@
-import Sample from 'Sample';
+import { hihat, kick, saw, snare, square, squareLead } from 'samples';
 import Song from 'Song';
 
 // Notes (it's more convenient _not_ to import them...)
@@ -25,49 +25,40 @@ const A5 = 70;
 const H5 = 72;
 const C6 = 73;
 
-const kick = new Sample([3,,0.11,0.49,0.34,0.05,,-0.24,0.2,,,-1,,0.45,0.16,,,,0.55,-0.75,,,,0.6]);
-const hihat = new Sample([3,,0.05,0.35,0.03,0.5,,,,,,-1,,0.45,0.16,,,,1,,,,,0.27]);
-const snare = new Sample([3,,0.25,0.49,0.34,0.25,,,,,,-1,,0.45,0.16,,,,1,,,,,0.33]);
-const square = new Sample([0,,0.16,,0.16,0.19,,0.02,-0.02,0.07,1,-1,,,-0.02,0.01,,,1,,,,,0.43]);
-const squareLead = new Sample([0,0.04,0.4,0.5497,0.1838,0.27,,,,,,,,,,,,,1,,,,,0.45]);
-const saw = new Sample([1,,0.1341,,0.1196,0.382,,,,,,,,0.0303,,,,,1,,,0.1,,0.3]);
+export const level1 = new Song({
+  bpm: 133,
+  channels: [
+    { sample: kick,       patterns: [1,1,1,1,1,1,1,1], notes: [C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,] },
+    { sample: hihat,      patterns: [1,1,1,1,1,1,1,1], notes: [,,C5,,,,C5,C5,,,C5,,,,C5,C5,,,C5,,,,C5,C5,,,C5,,,,C5,C5] },
+    { sample: snare,      patterns: [1,1,1,1,1,1,1,1], notes: [,,,,C5,,,,,,,,C5,,,,,,,,C5,,,,,,,,C5] },
+    { sample: snare,      patterns: [ ,1, , , , , , ], notes: [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,C5,C5,C5] },
+    { sample: snare,      patterns: [ , , ,1, ,1, ,1], notes: [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,E5] },
 
-export default {
-  level1: new Song({
-    bpm: 133,
-    channels: [
-      { sample: kick,       patterns: [1,1,1,1,1,1,1,1], notes: [C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,C5,,,,] },
-      { sample: hihat,      patterns: [1,1,1,1,1,1,1,1], notes: [,,C5,,,,C5,C5,,,C5,,,,C5,C5,,,C5,,,,C5,C5,,,C5,,,,C5,C5] },
-      { sample: snare,      patterns: [1,1,1,1,1,1,1,1], notes: [,,,,C5,,,,,,,,C5,,,,,,,,C5,,,,,,,,C5] },
-      { sample: snare,      patterns: [ ,1, , , , , , ], notes: [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,C5,C5,C5] },
-      { sample: snare,      patterns: [ , , ,1, ,1, ,1], notes: [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,E5] },
+    { sample: square,     patterns: [ , ,1,1,1,1, , ], notes: [0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,F3,F3,0,,F3,F3,0,,F3,F3,0,,F3,F3] },
+    { sample: square,     patterns: [ , ,1,1,1,1, , ], notes: [0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,C4,C4] },
+    { sample: square,     patterns: [ , , , , , ,1, ], notes: [0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3] },
+    { sample: square,     patterns: [ , , , , , ,1, ], notes: [0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,D4,D4] },
+    { sample: square,     patterns: [ , , , , , , ,1], notes: [0,,F3,F3,0,,F3,F3,0,,F3,F3,0,,F3,F3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3] },
+    { sample: square,     patterns: [ , , , , , , ,1], notes: [0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,D4,D4] },
 
-      { sample: square,     patterns: [ , ,1,1,1,1, , ], notes: [0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,F3,F3,0,,F3,F3,0,,F3,F3,0,,F3,F3] },
-      { sample: square,     patterns: [ , ,1,1,1,1, , ], notes: [0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,C4,C4] },
-      { sample: square,     patterns: [ , , , , , ,1, ], notes: [0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3] },
-      { sample: square,     patterns: [ , , , , , ,1, ], notes: [0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,D4,D4] },
-      { sample: square,     patterns: [ , , , , , , ,1], notes: [0,,F3,F3,0,,F3,F3,0,,F3,F3,0,,F3,F3,0,,G3,G3,0,,G3,G3,0,,G3,G3,0,,G3,G3] },
-      { sample: square,     patterns: [ , , , , , , ,1], notes: [0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,C4,C4,0,,D4,D4,0,,D4,D4,0,,D4,D4,0,,D4,D4] },
+    { sample: squareLead, patterns: [ , ,1, ,1, , , ], notes: [G5,,,,D5,,,,,,D5,,,,G5,,F5,,E5,,F5,,D5,,,,D5,,C5] },
+    { sample: squareLead, patterns: [ , , ,1, ,1, , ], notes: [H4,,,,G4,,,,,,G4,,H4,,G4,,F4,,C5,,D5,,E5,,,,E5,,D5] },
+    { sample: squareLead, patterns: [ , , , , ,1, , ], notes: [,C5,H4,0] },
+    { sample: squareLead, patterns: [ , , , , , ,1, ], notes: [C5,,,,G5,,E5,,,,C5,,,,C5,,G4,,D5,,C5,,H4,,,,G4,,A4,,H4] },
+    { sample: squareLead, patterns: [ , , , , , , ,1], notes: [F5,,F4,,E5,,F4,,D5,,F4,,C5,,F4,,H4,,C5,,D5,,G4] },
 
-      { sample: squareLead, patterns: [ , ,1, ,1, , , ], notes: [G5,,,,D5,,,,,,D5,,,,G5,,F5,,E5,,F5,,D5,,,,D5,,C5] },
-      { sample: squareLead, patterns: [ , , ,1, ,1, , ], notes: [H4,,,,G4,,,,,,G4,,H4,,G4,,F4,,C5,,D5,,E5,,,,E5,,D5] },
-      { sample: squareLead, patterns: [ , , , , ,1, , ], notes: [,C5,H4,0] },
-      { sample: squareLead, patterns: [ , , , , , ,1, ], notes: [C5,,,,G5,,E5,,,,C5,,,,C5,,G4,,D5,,C5,,H4,,,,G4,,A4,,H4] },
-      { sample: squareLead, patterns: [ , , , , , , ,1], notes: [F5,,F4,,E5,,F4,,D5,,F4,,C5,,F4,,H4,,C5,,D5,,G4] },
-
-      { sample: saw,        patterns: [ , ,1,1,1,1, , ], notes: [
-        G3, H3, D4, G4, D4, G4, H4, D5, H4, G4, H4, G4, D4, G4, D4, H3,
-        F3, H3, D4, F4, D4, F4, H4, D5, H4, F4, H4, F4, D4, F4, D4, H3,
-      ] },
-      { sample: saw,        patterns: [ , , , , , ,1, ], notes: [
-        E3, G3, C4, E4, C4, E4, G4, C5, G4, E4, G4, E4, C4, E4, C4, G3,
-        G3, H3, D4, G4, D4, G4, H4, D5, H4, G4, H4, G4, D4, G4, D4, H3,
-      ] },
-      { sample: saw,        patterns: [ , , , , , , ,1], notes: [
-        F3, A3, C4, F4, C4, F4, A4, C5, A4, F4, A4, F4, C4, F4, C4, A3,
-        G3, H3, D4, G4, D4, G4, H4, D5, H4, G4, H4, G4, D4, G4, D4, H3,
-      ] },
-    ],
-    loopAt: 2,
-  }),
-};
+    { sample: saw,        patterns: [ , ,1,1,1,1, , ], notes: [
+      G3, H3, D4, G4, D4, G4, H4, D5, H4, G4, H4, G4, D4, G4, D4, H3,
+      F3, H3, D4, F4, D4, F4, H4, D5, H4, F4, H4, F4, D4, F4, D4, H3,
+    ] },
+    { sample: saw,        patterns: [ , , , , , ,1, ], notes: [
+      E3, G3, C4, E4, C4, E4, G4, C5, G4, E4, G4, E4, C4, E4, C4, G3,
+      G3, H3, D4, G4, D4, G4, H4, D5, H4, G4, H4, G4, D4, G4, D4, H3,
+    ] },
+    { sample: saw,        patterns: [ , , , , , , ,1], notes: [
+      F3, A3, C4, F4, C4, F4, A4, C5, A4, F4, A4, F4, C4, F4, C4, A3,
+      G3, H3, D4, G4, D4, G4, H4, D5, H4, G4, H4, G4, D4, G4, D4, H3,
+    ] },
+  ],
+  loopAt: 2,
+});
